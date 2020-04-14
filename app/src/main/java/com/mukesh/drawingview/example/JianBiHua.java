a6bce8b17083ca23e9cf856aef5a979afe26a5b8
龚雨晨 <408536802@qq.com>
2020-04-14 17:34:12 +0800

    添加自动加载和切片算法

--- app/src/main/java/com/mukesh/drawingview/example/JianBiHua.java
+++ app/src/main/java/com/mukesh/drawingview/example/JianBiHua.java
@@ -1,12 +1,18 @@
 package com.mukesh.drawingview.example;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
+import android.widget.Toast;
 
 import com.mukesh.DrawingView;
 import com.pes.androidmaterialcolorpickerdialog.ColorPicker;
@@ -19,12 +25,36 @@ public class JianBiHua extends AppCompatActivity
     private Button saveButton, penButton, eraserButton, penColorButton, clearButton;
     private DrawingView drawingView;
     private SeekBar penSizeSeekBar, eraserSizeSeekBar;
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
 
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jianbihua);
         initializeUI();
         setListeners();
+        requestMyPermissions();//添加SD卡动态读写权力
+    }
+    private void requestMyPermissions() {
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            //没有授权，编写申请权限代码
+            ActivityCompat.requestPermissions(JianBiHua.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 100);
+        } else {
+            Log.d("TAG", "requestMyPermissions: 有写SD权限");
+        }
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            //没有授权，编写申请权限代码
+            ActivityCompat.requestPermissions(JianBiHua.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 100);
+        } else {
+            Log.d("TAG", "requestMyPermissions: 有读SD权限");
+        }
     }
 
     private void setListeners() {
@@ -47,6 +77,7 @@ public class JianBiHua extends AppCompatActivity
         eraserSizeSeekBar = findViewById(R.id.eraser_size_seekbar);
         clearButton = findViewById(R.id.clear_button);
     }
+    public native String stringFromJNI();
 
     @Override public void onClick(View view) {
         switch (view.getId()) {
@@ -54,6 +85,9 @@ public class JianBiHua extends AppCompatActivity
             case R.id.save_button:
                 drawingView.saveImage(Environment.getExternalStorageDirectory().toString(), "test",
                         Bitmap.CompressFormat.PNG, 100);
+                stringFromJNI();//进行切片操作，需要3个文件的路径，目前路径是写死的
+                Toast.makeText( getApplicationContext(),"切片成功",Toast.LENGTH_LONG ).show();
+
                 break;
                 //////////////////////////////////////////////////////////
             case R.id.pen_button:
--- app/src/main/java/com/mukesh/drawingview/example/ZhuJieMian.java
+++ app/src/main/java/com/mukesh/drawingview/example/ZhuJieMian.java
@@ -2,13 +2,24 @@ package com.mukesh.drawingview.example;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 //主界面跳转到四个分界面
 public class ZhuJieMian extends AppCompatActivity implements View.OnClickListener {
-
+  private static final String FILE_NAME[] = {
+          "box.STL",
+          "fdmextruder.def.json",
+          "fdmprinter.def.json"
+  };
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -21,8 +32,50 @@ public class ZhuJieMian extends AppCompatActivity implements View.OnClickListene
     findViewById(R.id.button2).setOnClickListener(this);
     findViewById(R.id.button3).setOnClickListener(this);
     findViewById(R.id.button4).setOnClickListener(this);
+    //将asserts文件夹下的文件加载至SD卡中（路径可以按你们需要改）
+    File testFolder = new File( Environment.getExternalStorageDirectory() + "/test");
+    if(testFolder.exists() && testFolder.isDirectory() ) {
+      Toast.makeText( getApplicationContext(),"资源文件已经存在",Toast.LENGTH_LONG ).show();
+    } else if(!testFolder.exists()) {
+      testFolder.mkdir();
+// check whether the model files exist in the phone **/
+// if not, copy them to there                       **/
+      for (int n =0; n < FILE_NAME.length; n++) {
+        File modelFile = new File(testFolder, FILE_NAME[n]);
+        if (!modelFile.exists()) {
+          copyAssetFilesToSDCard(modelFile, FILE_NAME[n]);
+        }
+      }
+      Toast.makeText( getApplicationContext(),"上传资源文件成功",Toast.LENGTH_LONG ).show();
+    }
   }
-//四个按钮分别实现四个分界面的跳转
+
+  private void copyAssetFilesToSDCard(final File testFileOnSdCard, final String FileToCopy) {
+    new Thread(new Runnable() {
+      @Override
+      public void run() {
+        try {
+          InputStream is = getAssets().open(FileToCopy);
+          FileOutputStream fos = new FileOutputStream(testFileOnSdCard);
+          byte[] buffer = new byte[8192];
+          int read;
+          try {
+            while ((read = is.read(buffer)) != -1) {
+              fos.write(buffer, 0, read);
+            }
+          } finally {
+            fos.flush();
+            fos.close();
+            is.close();
+          }
+        } catch (IOException e) {
+          Log.d("aaa", "Can't copy test file onto SD card");
+        }
+      }
+    }).start();
+  }
+
+  //四个按钮分别实现四个分界面的跳转
   @Override
   public void onClick(View view) {
     switch (view.getId()){
